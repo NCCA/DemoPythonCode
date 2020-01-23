dda7cb14aa2a81a98df241b5228e093ede4ae88e
jmacey <jmacey@bournemouth.ac.uk>
2020-01-23 10:02:27 +0000

    added a demo jupyter notebook based on the demos from the lab

--- Basic/enumerate.py
+++ Basic/enumerate.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python
-
+from __future__ import print_function
 colours=['red','green','blue','black','white']
 
 c=list(enumerate(colours))
-print c
+print(c)
 c=list((enumerate(colours,start=2)))
-print c
+print(c)
 
-for i in range(0,len(colours)) :
-	print i,colours[i]
+for number,colour in enumerate(colours) :
+	print (number,colour)
+print("use _ to ignore a value")
+for _,colour in enumerate(colours) :
+	print (colour)
--- Basic/set.py
+++ Basic/set.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python
-
+from __future__ import print_function
 a=range(0,5)
 a*=2
-print a
+print(a)
 b=set(a)
-print b
+print(b)
 
 
 a =set([1,2,3,4])
 b =set([3,4,5,6])
-print "a=",a
-print "b=",b
-print "union a | b",a | b
-print "intersection a & b" ,a & b
-print "subset false a<b",a < b
-print "difference a-b",a - b
-print "Symmetric diff a^b",a ^ b
+print ("a=",a)
+print ("b=",b)
+print ("union a | b",a | b)
+print ("intersection a & b" ,a & b)
+print ("subset false a<b",a < b)
+print ("difference a-b",a - b)
+print ("Symmetric diff a^b",a ^ b)
